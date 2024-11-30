4802252929bab77a41573ea1a2e54f40ae2997bb
Thadeus-Cruz <luthadcruz@gmail.com>
2024-11-30 12:25:48 +0530

    Message

--- src/main/java/com/example/autogarage/config/auto.java
+++ src/main/java/com/example/autogarage/config/auto.java
@@ -14,8 +14,8 @@ public class auto {
             @Override
             public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://auto-garage.netlify.app/")
-                        .allowedOrigins("http://13.234.157.28/") // Set allowed origins from environment variables
+                        .allowedOrigins("https://auto-garage.netlify.app/", "http://13.234.157.28/")
+                        // Set allowed origins from environment variables
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
